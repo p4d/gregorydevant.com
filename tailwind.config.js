9f573efe948efb01189499b253d0ba196f947fb7
Gregory Devant <gdevant@banque-edel.fr>
2020-09-10 23:04:15 +0200

    [ADD] Tailwind configuration and basic CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 module.exports = {
-  purge: [],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        'screen/2': '50vh',
+        'screen/3': 'calc(100vh / 3)',
+        'screen/4': 'calc(100vh / 4)',
+        'screen/5': 'calc(100vh / 5)',
+      }
+    }
   },
-  variants: {},
+  variants: { display: ["responsive", "hover", "focus"] },
   plugins: [],
 }
