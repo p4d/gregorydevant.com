f5fbb8c6dc78c2013f7fb713c8b84e24dcfc39c6
Gregory Devant <gdevant@banque-edel.fr>
2020-09-10 23:01:28 +0200

    [ADD] Add tailwind plugins, postcss and others

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,10 +1,30 @@
 // This is where project configuration and plugin options are located.
 // Learn more: https://gridsome.org/docs/config
 
-// Changes here require a server restart.
-// To restart press CTRL + C in terminal and run `gridsome develop`
+const tailwind = require('tailwindcss')
+const purgecss = require('@fullhuman/postcss-purgecss')
+
+const postcssPlugins = [
+  tailwind(),
+]
+
+if (process.env.NODE_ENV === 'production') postcssPlugins.push(purgecss(require('./purgecss.config.js')))
 
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
+    siteName: 'Gregory Devant - Portofolio',
+    css: {
+        loaderOptions: {
+            postcss: {
+                plugins: postcssPlugins,
+            },
+        },
+    },
+    plugins: [
+      {
+        use: 'gridsome-source-static-meta',
+        options: {
+          path: '~/data/*.json'
+        }
+      }
+    ]
 }
