41ab2ffd1be095e118e3386bfb59ae904f258ffa
Gregory Devant <gdevant@banque-edel.fr>
2020-09-10 23:06:40 +0200

    [ADD] Basic layout with navigation bar

--- src/layouts/Basic.vue
+++ src/layouts/Basic.vue
@@ -1,31 +1,37 @@
 <template>
-  <nav id="menu" class="navbar navbar-expand-md navbar-light bg-light">
-    <div class="container">
-      <a class="navbar-brand" href="#">Gregory Devant</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#technologies">Technologies</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#portofolio">Réalisations</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#a-propos">A Propos</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Contact</a>
-          </li>
-        </ul>
+  <div>
+    <nav id="menu" class="flex absolute w-full hidden items-center justify-between flex-wrap bg-teal-500 pb-4 pt-2">
+      <div class="flex items-center flex-shrink-0 text-white mr-6">
+        <span class="font-semibold text-xl tracking-tight">Grégory Devant</span>
       </div>
-    </div>
-  </nav>
+      <div class="block lg:hidden">
+        <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+          <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+        </button>
+      </div>
+      <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+        <div class="text-sm lg:flex-grow">
+          <a href="#" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+            Accueil
+          </a>
+          <a href="#technologies" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+            Technologies
+          </a>
+          <a href="#portofolio" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+            Réalisations
+          </a>
+          <a href="#a-propos" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+            A Propos
+          </a>
+          <a href="#contact" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
+            Contact
+          </a>
+        </div>
+      </div>
+    </nav>
+    <slot></slot>
+    <footer></footer>
+  </div>
 </template>
 
 <static-query>
@@ -44,13 +50,4 @@ body {
   line-height: 1.5;
 }
 
-nav#menu{
-  position: fixed;
-  width: 100%;
-  z-index: 2;
-  background: #fff !important;
-  padding-top: 5px;
-  padding-bottom: 5px;
-  top: -50px;
-}
 </style>
